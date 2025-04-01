f3586fa16924affff4066c93473a42dae0aa63d8
Mateo H <96021400+MateoHadzimustafic@users.noreply.github.com>
2025-04-01 15:26:40 -0400

    Updated Test Sensor example codes and added more libraries

--- tests/testbmp388.py
+++ tests/testbmp388.py
@@ -1,36 +1,38 @@
-from bmp388 import*
+from bmp388 import DFRobot_BMP388_SPI
 from machine import SPI, Pin
 import time
 
 
 # creates an object of the SPI
-spi = SPI(baudrate = 100000, polarity = 1, phase = 0, sck=Pin(18), mosi=Pin(19), miso=Pin(16))
+spi = SPI(1, baudrate = 100000, polarity = 0, phase = 0, sck=Pin(10), mosi=Pin(11), miso=Pin(8))
 # 0 - SPI bus at zero
 # baudrate = 1000000 - Baud rate to 1MHz (signal channels per second)
-# Polarity = 1 - idle state of the clock is high
+# Polarity = 0 - idle state of the clock is high
 # phase = 0 - data is sampled on the rising edge
 # bits = 8 - 8 bits per transfer
 # firstbit = SPI.MSB = most significant bit is transferred first
-# sck=Pin(18) - serial clock (timing for data transmission) at pin 18
-# mosi=Pin(19) - master out slave in (sending data from microcontroller to the rfm95)
-# miso=Pin(16) - master in slave out (recieving data from rfm95 to the mircocontroller)
+# sck=Pin(10) - serial clock (timing for data transmission) at pin 18
+# mosi=Pin(11) - master out slave in (sending data from microcontroller to the rfm95)
+# miso=Pin(8) - master in slave out (recieving data from rfm95 to the mircocontroller)
 
 
 # chip select Pin
-CS = Pin(26, Pin.OUT)
+CS = Pin(9, Pin.OUT)
 
 # creates an bmp388 object using the spi object and the chip select
-bmp388 = bmp388.DFRobot_BMP388_SPI(spi, CS)
+bmp388 = DFRobot_BMP388_SPI(spi, CS)
 
 
 while True:
     
-    print("Altitude : " + bmp388.readAltitude()) # prints the altitude (without calibration)
+    print("Altitude {:0.5f} ".format(bmp388.readAltitude())) # prints the altitude (without calibration)
     
-    time.sleep(0.5)
+    time.sleep(0.1)
     
     
     
+
+
 
 
 
--- tests/testbno055.py
+++ tests/testbno055.py
@@ -3,11 +3,11 @@ import time # used if you want to put delays in the code
 from bno055 import *  # imports micropython_bno055 Bosch library
 
 # Used to Communicate with the BNO055
-# 1 - i2c bus number
-# sda=machine.Pin(2) - sets the general input/output to the sda (serial data line)
-# scl-machine.Pin(3) - sets the general input/output to the scl (serial clock line)
+# 0 - i2c bus number
+# sda=machine.Pin(4) - sets the general input/output to the sda (serial data line)
+# scl-machine.Pin(5) - sets the general input/output to the scl (serial clock line)
 # timeout=100_000 - timeout for the bno055 sensor by 100000 microseconds 
-i2c = machine.I2C(1, sda=machine.Pin(2), scl=machine.Pin(3), timeout=100_000) #
+i2c = machine.I2C(0, sda=machine.Pin(4), scl=machine.Pin(5), timeout=100_000) #
 
 # imu is an instance of hte BNO055 sensor
 # Call methods on the imu
@@ -21,19 +21,21 @@ calibrated = False
 
 while True:
     
-    time.sleep(1)
-    if not calibrated
+    time.sleep(0.1)
+    if not calibrated:
         calibrated = imu.calibrated() # shows if the sensor is fully calibrated
     
     # Test prints of Temperature, Gryo, Acceleration, Linear Acceleration, and Quaternions
     print('Temperature: {} Celcius'.format(imu.temperature())) # Temperature of the bno055 sensor
-    print('Gryo: x: {:5.0f}  x: {:5.0f}  x: {:5.0f}'.format(*imu.gryo())) # Gryo of the bno055 sensor
+    print('Gyro: x: {:5.0f}  x: {:5.0f}  x: {:5.0f}'.format(*imu.gyro())) # Gryo of the bno055 sensor
     print('Acceleration: x:{:5.1f}  y:{:5.1f} z:{:5.1f}'.format(*imu.accel())) # Acceleration of the bno055 sensor
     print('Linear Accelertaion: x{:5.0f}  y:{:5.1f}  z: {:5.1f}'.format(*imu.lin_acc())) # Linear Acceleration of the bno055 sensor
-    print('Quaternions: w: {0:5f} x {0.5f} y {0.5f} z{0.5f}'.format(*imu.quaternion())) # Quaternions of the bno055 sensor 
+    print('Quaternions: w: {:0.5f} x {:0.5f} y {:0.5f} z{:0.5f}'.format(*imu.quaternion())) # Quaternions of the bno055 sensor 
+    print('-----------------------------------------------------------------------')
     
     
-    
+
+
 
 
 
