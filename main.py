3a73f2bfc49602d6743da3ad02de0a57fe1d29da
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2025-04-02 14:42:40 -0400

    chore: Remove LED code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from src.flight import FlightComputer
-from src.led import change_led
 from lib.icm42670 import read_who_am_i, configure_sensor, set_accel_scale, set_gyro_scale
 
 import asyncio
@@ -17,9 +16,8 @@ async def main():
 
     gps_task = asyncio.create_task(flight.poll_gps())
     transmit_task = asyncio.create_task(flight.transmit())
-    led_task = asyncio.create_task(change_led())
 
-    await asyncio.gather(gps_task, transmit_task, led_task)
+    await asyncio.gather(gps_task, transmit_task)
 
 
 if __name__ == '__main__':
